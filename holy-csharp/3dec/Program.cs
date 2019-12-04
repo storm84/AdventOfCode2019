67c433ed8d4c9662b332c07851507ee0ea571deb
TheHolyNoah <58525871+TheHolyNoah@users.noreply.github.com>
2019-12-04 20:17:17 +0100

    Add files via upload

---
+++ holy-csharp/3dec/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3dec
+{
+    class Program
+    {
+        static Dictionary<Tuple<int, int>, int> getPath(string input)
+        {
+            Dictionary<Tuple<int, int>, int> thisPath = new Dictionary<Tuple<int, int>, int>();
+
+            int x = 0, y = 0, totLength = 0;
+
+            foreach (var p in input.Split(','))
+            {
+                var direction = p[0];
+                var length = int.Parse(p.Remove(0, 1));
+
+                for (int i = 0; i < length; i++)
+                {
+                    totLength++;
+                    if (direction == 'U')
+                    {
+                        Tuple<int, int> thisTuple = new Tuple<int, int>(x, ++y);
+                        if (!thisPath.ContainsKey(thisTuple))
+                            thisPath.Add(thisTuple, totLength);
+                    }
+                    else if (direction == 'D')
+                    {
+                        Tuple<int, int> thisTuple = new Tuple<int, int>(x, --y);
+                        if (!thisPath.ContainsKey(thisTuple))
+                            thisPath.Add(thisTuple, totLength);
+                    }
+                    else if (direction == 'R')
+                    {
+                        Tuple<int, int> thisTuple = new Tuple<int, int>(++x, y);
+                        if (!thisPath.ContainsKey(thisTuple))
+                            thisPath.Add(thisTuple, totLength);
+                    }
+                    else if (direction == 'L')
+                    {
+                        Tuple<int, int> thisTuple = new Tuple<int, int>(--x, y);
+                        if (!thisPath.ContainsKey(thisTuple))
+                            thisPath.Add(thisTuple, totLength);
+                    }
+                    else
+                    {
+                        // Should never get here!!!
+                        Console.WriteLine("Warning!!! Unknown direction " + direction);
+                        return null;
+                    }
+
+                }
+            }
+
+            return thisPath;
+        }
+
+        static int findMinDist(Tuple<int, int>[] intersections)
+        {
+            int dist = int.MaxValue;
+            foreach (Tuple<int, int> p in intersections)
+            {
+                if ((Math.Abs(p.Item1) + Math.Abs(p.Item2)) < dist)
+                    dist = (Math.Abs(p.Item1) + Math.Abs(p.Item2));
+            }
+
+            return dist;
+        }
+
+        static int findMinDelay(Tuple<int, int>[] intersections, Dictionary<Tuple<int, int>, int>[] paths)
+        {
+            int delay = int.MaxValue;
+            foreach (Tuple<int, int> p in intersections)
+            { 
+                int[] delays = new int[2];
+                paths[0].TryGetValue(p, out delays[0]);
+                paths[1].TryGetValue(p, out delays[1]);
+                if ((delays[0] + delays[1]) < delay)
+                    delay = delays[0] + delays[1];
+            }
+
+            return delay;
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Neeeeeeded a bigger buffer for input
+            byte[] inputBuffer = new byte[10000];
+            Stream inputStream = Console.OpenStandardInput(inputBuffer.Length);
+            Console.SetIn(new StreamReader(inputStream, Console.InputEncoding, false, inputBuffer.Length));
+
+            // Get input
+            string[] input = new string[2];
+            input[0] = Console.ReadLine();
+            input[1] = Console.ReadLine();
+
+            //Console.WriteLine("1st - " + input[0]);
+            //Console.WriteLine("2nd - " + input[1]);
+
+            // Translate into paths
+            Dictionary<Tuple<int, int>, int>[] paths = new Dictionary<Tuple<int, int>, int>[2];
+            paths[0] = getPath(input[0]);
+            paths[1] = getPath(input[1]);
+
+
+            // Find intersections 
+            Tuple<int, int>[] intersections = paths[0].Keys.Intersect(paths[1].Keys).ToArray();
+
+            // Find min distance
+            Console.WriteLine("Min dist:  " + findMinDist(intersections));
+            Console.WriteLine("Min delay: " + findMinDelay(intersections, paths)); 
+
+            Console.Read();
+        }
+    }
+}
---
+++ holy-csharp/4dec/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4dec
+{
+    class Program
+    {
+        static bool check6Digits(int value)
+        {
+            //Console.WriteLine("Value and is6: " + value + " " + (value.ToString().Length == 6));
+            return (value.ToString().Length == 6);
+        }
+
+        // Behöver ha minst 2 av samma nummer, men i kombination med alltid ökande, så måste dessa hamna bredvid varandra
+        static bool checkAtLeast2SameDigits(int value)
+        {
+            string valueStr = value.ToString();
+
+            foreach(char a in valueStr)
+            {
+                if ((valueStr.Length - valueStr.Replace(a.ToString(), "").Length) > 1)
+                    return true;
+            }
+
+            return false;
+        }
+
+        // Behöver ha exakt 2 av samma nummer, men i kombination med alltid ökande, så måste dessa hamna bredvid varandra
+        static bool checkExact2SameDigits(int value)
+        {
+            string valueStr = value.ToString();
+
+            foreach (char a in valueStr)
+            {
+                if ((valueStr.Length - valueStr.Replace(a.ToString(), "").Length) == 2)
+                    return true;
+            }
+
+            return false;
+        }
+
+
+        static bool checkAlwaysIncrease(int value)
+        {
+            string valueStr = value.ToString();
+
+            for(int i = 1; i < valueStr.Length; i++)
+            {
+                if (valueStr[i] < valueStr[i - 1])
+                    return false;
+            }
+
+            return true;
+        }
+
+        static void solve(string input)
+        {
+            int min = int.Parse(input.Split('-')[0]);
+            int max = int.Parse(input.Split('-')[1]);
+
+            Console.WriteLine("Min = " + min + " and Max = " + max);
+
+            // Task 1
+            int counter = 0;
+            for (int i = min; i <= max; i++)
+            {
+                if (check6Digits(i) && checkAtLeast2SameDigits(i) && checkAlwaysIncrease(i))
+                    counter++;
+            }
+            Console.WriteLine("1: Amount of possible passwords: " + counter);
+
+            // Task 2
+            counter = 0;
+            for (int i = min; i <= max; i++)
+            {
+                if (check6Digits(i) && checkExact2SameDigits(i) && checkAlwaysIncrease(i))
+                    counter++;
+            }
+
+            Console.WriteLine("2: Amount of possible passwords: " + counter);
+
+            Console.Read();
+        }
+
+        static void Main(string[] args)
+        {
+            // Get input
+            string input = Console.ReadLine();
+
+            solve(input);
+        }
+    }
+}
